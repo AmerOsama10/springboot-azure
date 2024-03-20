d0b036a64f4f6ce38f01ecccf8e77285131f1ff1
AmerOsama10 <amerosama.jr@gmail.com>
2024-03-20 04:11:48 +0200

    add wild fly proto

---
+++ spring-azure-wildfly/src/main/java/com/unilever/azureauth/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.unilever.azureauth.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http.csrf(t -> t.disable());
+		http.authorizeHttpRequests(auth -> {
+			auth.requestMatchers("/").permitAll();
+			auth.anyRequest().authenticated();
+		})
+		.oauth2Login()
+		.defaultSuccessUrl("/welcome",true);
+
+
+		return http.build();
+	}
+	
+
+
+}
